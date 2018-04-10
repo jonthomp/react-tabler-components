2ac25d65ad60d666a8eddb52e8b75a7a7fb32b63
Jon Thompson <jon@jont.me>
2018-04-10 22:31:24 +0100

    Table: adds responsive option

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -2,23 +2,31 @@ import React from "react";
 import PropTypes from "prop-types";
 import cn from "classnames";
 
-const Table = ({ className, cards, striped, ...props }) => {
+const Table = ({ className, cards, striped, responsive, ...props }) => {
   const classes = cn(
     "table",
     { "card-table": !!cards, "table-striped": !!striped },
     className
   );
-  return <table className={classes} {...props} />;
+  return !responsive ? (
+    <table className={classes} {...props} />
+  ) : (
+    <div className="table-responsive">
+      <table className={classes} {...props} />
+    </div>
+  );
 };
 
 Table.propTypes = {
   cards: PropTypes.bool,
-  striped: PropTypes.bool
+  striped: PropTypes.bool,
+  responsive: PropTypes.bool
 };
 
 Table.defaultProps = {
   cards: false,
-  striped: false
+  striped: false,
+  responsive: false
 };
 
 export default Table;
