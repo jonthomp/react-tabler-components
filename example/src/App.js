218fe167b5e0e0bbbdd055ee75dadc85be0f87d0
Jon Thompson <jon@jont.me>
2018-04-11 00:07:10 +0100

    Add Alerts and Progress to demo

--- example/src/App.js
+++ example/src/App.js
@@ -14,7 +14,9 @@ import {
   Card,
   Text,
   Header,
-  Table
+  Table,
+  Alert,
+  Progress
 } from "react-tabler-components";
 
 class App extends Component {
@@ -208,7 +210,7 @@ class App extends Component {
                   </Card.Body>
                 </Card>
               </Grid.Col>
-              <div className="col-lg-6">
+              <Grid.Col lg={6}>
                 <Card>
                   <Card.Header>
                     <Card.Title>Development Activity</Card.Title>
@@ -217,101 +219,233 @@ class App extends Component {
                     id="chart-development-activity"
                     style={{ height: "10rem" }}
                   />
-                  <div className="table-responsive">
-                    <Table
-                      cards={true}
-                      striped={true}
-                      className="table-vcenter"
-                    >
-                      <Table.Header>
-                        <Table.Row>
-                          <th colspan="2">User</th>
-                          <th>Commit</th>
-                          <th>Date</th>
-                          <th />
-                        </Table.Row>
-                      </Table.Header>
-                      <Table.Body>
-                        <Table.Row>
-                          <Table.Col className="w-1">
-                            <Avatar src="./demo/faces/male/9.jpg" />
-                          </Table.Col>
-                          <Table.Col>Ronald Bradley</Table.Col>
-                          <Table.Col>Initial commit</Table.Col>
-                          <Table.Col className="text-nowrap">
-                            May 6, 2018
-                          </Table.Col>
-                          <Table.Col className="w-1">
-                            <a href="#" className="icon">
-                              <i className="fe fe-trash" />
-                            </a>
-                          </Table.Col>
-                        </Table.Row>
-                        <Table.Row>
-                          <Table.Col>
-                            <Avatar>BM</Avatar>
-                          </Table.Col>
-                          <Table.Col>Russell Gibson</Table.Col>
-                          <Table.Col>Main structure</Table.Col>
-                          <Table.Col className="text-nowrap">
-                            April 22, 2018
-                          </Table.Col>
-                          <Table.Col>
-                            <a href="#" className="icon">
-                              <i className="fe fe-trash" />
-                            </a>
-                          </Table.Col>
-                        </Table.Row>
-                        <Table.Row>
-                          <Table.Col>
-                            <Avatar src="./demo/faces/female/1.jpg" />
-                          </Table.Col>
-                          <Table.Col>Beverly Armstrong</Table.Col>
-                          <Table.Col>Left sidebar adjustments</Table.Col>
-                          <Table.Col className="text-nowrap">
-                            April 15, 2018
-                          </Table.Col>
-                          <Table.Col>
-                            <a href="#" className="icon">
-                              <i className="fe fe-trash" />
-                            </a>
-                          </Table.Col>
-                        </Table.Row>
-                        <Table.Row>
-                          <Table.Col>
-                            <Avatar src="./demo/faces/male/4.jpg" />
-                          </Table.Col>
-                          <Table.Col>Bobby Knight</Table.Col>
-                          <Table.Col>Topbar dropdown style</Table.Col>
-                          <Table.Col className="text-nowrap">
-                            April 8, 2018
-                          </Table.Col>
-                          <Table.Col>
-                            <a href="#" className="icon">
-                              <i className="fe fe-trash" />
-                            </a>
-                          </Table.Col>
-                        </Table.Row>
-                        <Table.Row>
-                          <Table.Col>
-                            <Avatar src="./demo/faces/female/11.jpg" />
-                          </Table.Col>
-                          <Table.Col>Sharon Wells</Table.Col>
-                          <Table.Col>Fixes #625</Table.Col>
-                          <Table.Col className="text-nowrap">
-                            April 9, 2018
-                          </Table.Col>
-                          <Table.Col>
-                            <a href="#" className="icon">
-                              <i className="fe fe-trash" />
-                            </a>
-                          </Table.Col>
-                        </Table.Row>
-                      </Table.Body>
-                    </Table>
+                  <Table
+                    cards={true}
+                    striped={true}
+                    responsive={true}
+                    className="table-vcenter"
+                  >
+                    <Table.Header>
+                      <Table.Row>
+                        <Table.ColHeader colspan="2">User</Table.ColHeader>
+                        <Table.ColHeader>Commit</Table.ColHeader>
+                        <Table.ColHeader>Date</Table.ColHeader>
+                        <Table.ColHeader />
+                      </Table.Row>
+                    </Table.Header>
+                    <Table.Body>
+                      <Table.Row>
+                        <Table.Col className="w-1">
+                          <Avatar src="./demo/faces/male/9.jpg" />
+                        </Table.Col>
+                        <Table.Col>Ronald Bradley</Table.Col>
+                        <Table.Col>Initial commit</Table.Col>
+                        <Table.Col className="text-nowrap">
+                          May 6, 2018
+                        </Table.Col>
+                        <Table.Col className="w-1">
+                          <Icon link={true} href="#" name="trash" />
+                        </Table.Col>
+                      </Table.Row>
+                      <Table.Row>
+                        <Table.Col>
+                          <Avatar>BM</Avatar>
+                        </Table.Col>
+                        <Table.Col>Russell Gibson</Table.Col>
+                        <Table.Col>Main structure</Table.Col>
+                        <Table.Col className="text-nowrap">
+                          April 22, 2018
+                        </Table.Col>
+                        <Table.Col>
+                          <Icon link={true} href="#" name="trash" />
+                        </Table.Col>
+                      </Table.Row>
+                      <Table.Row>
+                        <Table.Col>
+                          <Avatar src="./demo/faces/female/1.jpg" />
+                        </Table.Col>
+                        <Table.Col>Beverly Armstrong</Table.Col>
+                        <Table.Col>Left sidebar adjustments</Table.Col>
+                        <Table.Col className="text-nowrap">
+                          April 15, 2018
+                        </Table.Col>
+                        <Table.Col>
+                          <Icon link={true} href="#" name="trash" />
+                        </Table.Col>
+                      </Table.Row>
+                      <Table.Row>
+                        <Table.Col>
+                          <Avatar src="./demo/faces/male/4.jpg" />
+                        </Table.Col>
+                        <Table.Col>Bobby Knight</Table.Col>
+                        <Table.Col>Topbar dropdown style</Table.Col>
+                        <Table.Col className="text-nowrap">
+                          April 8, 2018
+                        </Table.Col>
+                        <Table.Col>
+                          <Icon link={true} href="#" name="trash" />
+                        </Table.Col>
+                      </Table.Row>
+                      <Table.Row>
+                        <Table.Col>
+                          <Avatar src="./demo/faces/female/11.jpg" />
+                        </Table.Col>
+                        <Table.Col>Sharon Wells</Table.Col>
+                        <Table.Col>Fixes #625</Table.Col>
+                        <Table.Col className="text-nowrap">
+                          April 9, 2018
+                        </Table.Col>
+                        <Table.Col>
+                          <Icon link={true} href="#" name="trash" />
+                        </Table.Col>
+                      </Table.Row>
+                    </Table.Body>
+                  </Table>
+                </Card>
+              </Grid.Col>
+
+              <Grid.Col md={6}>
+                <Alert primary={true}>
+                  Are you in trouble?{" "}
+                  <Alert.Link href="./docs/index.html">
+                    Read our documentation
+                  </Alert.Link>{" "}
+                  with code samples.
+                </Alert>
+                <Grid.Row>
+                  <Grid.Col sm={6}>
+                    <Card>
+                      <Card.Header>
+                        <Card.Title>Chart title</Card.Title>
+                      </Card.Header>
+                      <Card.Body>
+                        <div id="chart-donut" style={{ height: "12rem" }} />
+                      </Card.Body>
+                    </Card>
+                  </Grid.Col>
+                  <Grid.Col sm={6}>
+                    <Card>
+                      <Card.Header>
+                        <Card.Title>Chart title</Card.Title>
+                      </Card.Header>
+                      <Card.Body>
+                        <div id="chart-pie" style={{ height: "12rem" }} />
+                      </Card.Body>
+                    </Card>
+                  </Grid.Col>
+                  <Grid.Col sm={4}>
+                    <Card>
+                      <Card.Body className="text-center">
+                        <Header size={5}>New feedback</Header>
+                        <div className="display-4 font-weight-bold mb-4">
+                          62
+                        </div>
+                        <Progress size="sm">
+                          <Progress.Bar color="red" width={28} />
+                        </Progress>
+                      </Card.Body>
+                    </Card>
+                  </Grid.Col>
+                  <Grid.Col sm={4}>
+                    <Card>
+                      <Card.Body className="text-center">
+                        <Header size={5}>Today profit</Header>
+                        <div className="display-4 font-weight-bold mb-4">
+                          $652
+                        </div>
+                        <Progress size="sm">
+                          <Progress.Bar color="green" width={84} />
+                        </Progress>
+                      </Card.Body>
+                    </Card>
+                  </Grid.Col>
+                  <Grid.Col sm={4}>
+                    <Card>
+                      <Card.Body className="text-center">
+                        <Header size={5}>Users online</Header>
+                        <div className="display-4 font-weight-bold mb-4">
+                          76
+                        </div>
+                        <Progress size="sm">
+                          <Progress.Bar color="yellow" width={34} />
+                        </Progress>
+                      </Card.Body>
+                    </Card>
+                  </Grid.Col>
+                </Grid.Row>
+              </Grid.Col>
+              <Grid.Col sm={6} lg={3}>
+                <Card className="p-3">
+                  <div className="d-flex align-items-center">
+                    <span className="stamp stamp-md bg-blue mr-3">
+                      <Icon name="dollar-sign" />
+                    </span>
+                    <div>
+                      <h4 className="m-0">
+                        <a href="javascript:void(0)">
+                          132
+                          <small>Sales</small>
+                        </a>
+                      </h4>
+                      <small className="text-muted">12 waiting payments</small>
+                    </div>
                   </div>
                 </Card>
-              </div>
+              </Grid.Col>
+              <Grid.Col sm={6} lg={3}>
+                <Card className="p-3">
+                  <div className="d-flex align-items-center">
+                    <span className="stamp stamp-md bg-green mr-3">
+                      <Icon name="shopping-cart" />
+                    </span>
+                    <div>
+                      <h4 className="m-0">
+                        <a href="javascript:void(0)">
+                          78
+                          <small>Orders</small>
+                        </a>
+                      </h4>
+                      <small className="text-muted">32 shipped</small>
+                    </div>
+                  </div>
+                </Card>
+              </Grid.Col>
+              <Grid.Col sm={6} lg={3}>
+                <Card className="p-3">
+                  <div className="d-flex align-items-center">
+                    <span className="stamp stamp-md bg-red mr-3">
+                      <Icon name="users" />
+                    </span>
+                    <div>
+                      <h4 className="m-0">
+                        <a href="javascript:void(0)">
+                          1,352
+                          <small>Members</small>
+                        </a>
+                      </h4>
+                      <small className="text-muted">163 registered today</small>
+                    </div>
+                  </div>
+                </Card>
+              </Grid.Col>
+              <Grid.Col sm={6} lg={3}>
+                <Card className="p-3">
+                  <div className="d-flex align-items-center">
+                    <span className="stamp stamp-md bg-yellow mr-3">
+                      <i className="fe fe-message-square" />
+                    </span>
+                    <div>
+                      <h4 className="m-0">
+                        <a href="javascript:void(0)">
+                          132
+                          <small>Comments</small>
+                        </a>
+                      </h4>
+                      <small className="text-muted">16 waiting</small>
+                    </div>
+                  </div>
+                </Card>
+              </Grid.Col>
             </Grid.Row>
           </Page.Content>
         </Page.Main>
