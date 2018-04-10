b6d84ccf0bdd70dad5eeadc75a612cc75ab521a7
Jon Thompson <jon@jont.me>
2018-04-10 22:00:33 +0100

    Icon: add link option

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -1,17 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Icon = ({ name, className, ...rest }) => (
-  <i className={`fe fe-${name} ${className}`} />
-);
+const Icon = ({ name, className, link, ...rest }) =>
+  !link ? (
+    <i className={`fe fe-${name} ${className}`} />
+  ) : (
+    <a className={`icon ${className}`} {...rest}>
+      <i className={`fe fe-${name}`} />
+    </a>
+  );
 
 Icon.propTypes = {
   name: PropTypes.string.isRequired,
-  className: PropTypes.string
+  className: PropTypes.string,
+  link: PropTypes.bool
 };
 
 Icon.defaultProps = {
-  className: ""
+  className: "",
+  link: false
 };
 
 export default Icon;
