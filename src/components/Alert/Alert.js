f676fbe56e9aa2c3f638232c2cd742286976cf59
Jon Thompson <jon@jont.me>
2018-04-11 21:53:09 +0100

    Alert: adds extraSpace, dismissible and avatar options

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import cn from "classnames";
-import { Icon } from "../";
+import { Icon, Avatar } from "../";
 
 const Alert = ({
   className,
@@ -13,23 +13,33 @@ const Alert = ({
   warning,
   danger,
   icon,
+  extraSpace,
+  dismissible,
+  avatar,
   ...props
 }) => {
   const classes = cn(
     `alert`,
     {
-      "alert-primary": !!primary,
-      "alert-secondary": !!secondary,
-      "alert-success": !!success,
-      "alert-info": !!info,
-      "alert-warning": !!warning,
-      "alert-danger": !!danger,
-      "alert-icon": !!icon
+      "alert-primary": primary,
+      "alert-secondary": secondary,
+      "alert-success": success,
+      "alert-info": info,
+      "alert-warning": warning,
+      "alert-danger": danger,
+      "alert-icon": !!icon,
+      "mt-5 mb-6": extraSpace,
+      "alert-dismissible": dismissible,
+      "alert-avatar": !!avatar
     },
     className
   );
   return (
     <div className={classes} role="alert" {...props}>
+      {dismissible && (
+        <button type="button" className="close" data-dismiss="alert" />
+      )}
+      {avatar && <Avatar src={avatar} />}
       {icon && <Icon name={icon} className="mr-2" aria-hidden="true" />}
       {children}
     </div>
@@ -44,7 +54,10 @@ Alert.propTypes = {
   info: PropTypes.bool,
   warning: PropTypes.bool,
   danger: PropTypes.bool,
-  icon: PropTypes.string
+  icon: PropTypes.string,
+  extraSpace: PropTypes.bool,
+  dismissible: PropTypes.bool,
+  avatar: PropTypes.string
 };
 
 Alert.defaultProps = {
@@ -55,7 +68,10 @@ Alert.defaultProps = {
   info: false,
   warning: false,
   danger: false,
-  icon: null
+  icon: null,
+  extraSpace: false,
+  dismissible: false,
+  avatar: null
 };
 
 export default Alert;
