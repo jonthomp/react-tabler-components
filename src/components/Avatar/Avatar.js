881508d6bbfd357383ecbdc47466d07414481fc7
Jon Thompson <jon@jont.me>
2018-04-11 23:09:39 +0100

    Avatar: adds size, status, placeholder, icon and color options

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -1,34 +1,71 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Icon } from "../";
+import cn from "classnames";
 
-const Avatar = ({ className, image, style, ...rest }) => (
-  <span
-    className={`avatar ${className}`}
-    style={
-      image
-        ? Object.assign(
-            {
-              backgroundImage: `url(${image})`
-            },
-            style
-          )
-        : style
-    }
-    {...rest}
-  />
-);
+const Avatar = ({
+  className,
+  children,
+  image,
+  style,
+  size,
+  status,
+  placeholder,
+  icon,
+  color,
+  ...rest
+}) => {
+  const classes = cn(
+    {
+      avatar: true,
+      [`avatar-${size}`]: !!size,
+      "avatar-placeholder": placeholder,
+      [`avatar-${color}`]: !!color
+    },
+    className
+  );
+  return (
+    <span
+      className={classes}
+      style={
+        image
+          ? Object.assign(
+              {
+                backgroundImage: `url(${image})`
+              },
+              style
+            )
+          : style
+      }
+      {...rest}
+    >
+      {icon && <Icon name={icon} />}
+      {status && <span className={`avatar-status bg-${status}`} />}
+      {children}
+    </span>
+  );
+};
 
 Avatar.propTypes = {
   className: PropTypes.string,
   image: PropTypes.string,
-  style: PropTypes.object
+  style: PropTypes.object,
+  size: PropTypes.oneOf("sm", "md", "lg", "xl", "xxl"),
+  status: PropTypes.oneOf("grey", "red", "yellow", "green"),
+  placeholder: PropTypes.bool,
+  icon: PropTypes.string,
+  color: PropTypes.string
 };
 
 Avatar.defaultProps = {
   className: "",
   image: null,
-  style: {}
+  style: {},
+  size: null,
+  status: null,
+  placeholder: false,
+  icon: null,
+  color: null
 };
 
 export default Avatar;
