6bd794184c96aec5f0027d2deba0e9c8103e101d
Jon Thompson <jon@jont.me>
2018-04-11 12:41:41 +0100

    Alert: adds color and icon options

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -1,20 +1,50 @@
 import React from "react";
 import PropTypes from "prop-types";
 import cn from "classnames";
+import { Icon } from "../";
 
-const Alert = ({ className, primary, ...props }) => {
-  const classes = cn(`alert`, { "alert-primary": !!primary }, className);
-  return <div className={classes} {...props} />;
+const Alert = ({ className, children, primary, icon, ...props }) => {
+  const classes = cn(
+    `alert`,
+    {
+      "alert-primary": !!primary,
+      "alert-secondary": !!secondary,
+      "alert-success": !!success,
+      "alert-info": !!info,
+      "alert-warning": !!warning,
+      "alert-danger": !!danger,
+      "alert-icon": !!icon
+    },
+    className
+  );
+  return (
+    <div className={classes} role="alert" {...props}>
+      {icon && <Icon name={icon} className="mr-2" aria-hidden="true" />}
+      {children}
+    </div>
+  );
 };
 
 Alert.propTypes = {
   className: PropTypes.string,
-  primary: PropTypes.bool
+  primary: PropTypes.bool,
+  secondary: PropTypes.bool,
+  success: PropTypes.bool,
+  info: PropTypes.bool,
+  warning: PropTypes.bool,
+  danger: PropTypes.bool,
+  icon: PropTypes.string
 };
 
 Alert.defaultProps = {
   className: "",
-  primary: false
+  primary: false,
+  secondary: false,
+  success: false,
+  info: false,
+  warning: false,
+  danger: false,
+  icon: null
 };
 
 export default Alert;
