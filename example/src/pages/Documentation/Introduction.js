d7a068af83f5995a223dba327bc34752581566ed
Jon Thompson <jon@jont.me>
2018-04-11 19:51:31 +0100

    Docs: Intro placholder

--- example/src/pages/Documentation/Introduction.js
+++ example/src/pages/Documentation/Introduction.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 
-import { Page } from "react-tabler-components";
+import { Page, Text, Header } from "react-tabler-components";
 
 import DocsWrapper from "./DocsWrapper";
 
 class Documentation extends Component {
   render() {
-    return <DocsWrapper title="Documentation">{"content"}</DocsWrapper>;
+    return (
+      <DocsWrapper title="Documentation">
+        <Header.H2>Introduction</Header.H2>
+        <p>A set of React components for Tabler</p>
+      </DocsWrapper>
+    );
   }
 }
 export default Documentation;
