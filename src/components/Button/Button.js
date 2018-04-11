09cf3fbc05b006a61dbfc0d0497a233ad804f9bd
Jon Thompson <jon@jont.me>
2018-04-11 18:25:54 +0100

    Button: outline/block options

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,18 +1,40 @@
 import React from "react";
 import PropTypes from "prop-types";
+import cn from "classnames";
 
-const Button = ({ as: Component, size, ...rest }) => (
-  <Component className={`btn btn-${size} btn-outline-primary`} {...rest} />
-);
+const Button = ({
+  as: Component,
+  size,
+  outline,
+  primary,
+  className,
+  ...rest
+}) => {
+  const classes = cn(
+    {
+      btn: true,
+      [`btn-${size}`]: !!size,
+      [`btn-outline-primary`]: outline && primary,
+      [`btn-primary`]: primary,
+      [`btn-block`]: primary && !outline
+    },
+    className
+  );
+  return <Component className={classes} {...rest} />;
+};
 
 Button.propTypes = {
   as: PropTypes.node,
-  size: PropTypes.string
+  size: PropTypes.string,
+  primary: PropTypes.bool,
+  outline: PropTypes.bool
 };
 
 Button.defaultProps = {
   as: "a",
-  size: "sm"
+  size: null,
+  primary: true,
+  outline: false
 };
 
 export default Button;
