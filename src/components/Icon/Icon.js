94d53bbea7f7d82d3e48df347f38073fa9a068df
Jon Thompson <jon@jont.me>
2018-04-12 02:12:40 +0100

    Icon: adds option to change prfix

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -1,24 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Icon = ({ name, className, link, ...rest }) =>
+const Icon = ({ prefix, name, className, link, ...rest }) =>
   !link ? (
-    <i className={`fe fe-${name} ${className}`} />
+    <i className={`${prefix} ${prefix}-${name} ${className}`} />
   ) : (
     <a className={`icon ${className}`} {...rest}>
-      <i className={`fe fe-${name}`} />
+      <i className={`${prefix} ${prefix}-${name}`} />
     </a>
   );
 
 Icon.propTypes = {
   name: PropTypes.string.isRequired,
   className: PropTypes.string,
-  link: PropTypes.bool
+  link: PropTypes.bool,
+  prefix: PropTypes.oneOf("fa", "fe")
 };
 
 Icon.defaultProps = {
   className: "",
-  link: false
+  link: false,
+  prefix: "fe"
 };
 
 export default Icon;
