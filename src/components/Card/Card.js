149aae0d563b91370773e689e559ede174c60799
Jon Thompson <jon@jont.me>
2018-04-13 11:26:04 +0100

    Adds component from as prop and format with prettier

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,41 +5,38 @@ import CardHeader from "./CardHeader";
 import CardTitle from "./CardTitle";
 import CardBody from "./CardBody";
 
-const Card = ({className, title, body, ...props}) => {
-    const classes = cn("card", className);
-    const card_header = title === '' ? null : <CardHeader><CardTitle>{title}</CardTitle></CardHeader>;
-    const card_body = body === null ? null : <CardBody>{body}</CardBody>;
+const Card = ({ className, as: Component, title, body, ...props }) => {
+  const classes = cn("card", className);
+  const card_header =
+    title === "" ? null : (
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+      </CardHeader>
+    );
+  const card_body = body === null ? null : <CardBody>{body}</CardBody>;
 
-    if (card_header !== null || card_body !== null) {
-        return (
-            <div className={classes} {...props} >
-                {card_header}
-                {card_body}
-            </div>
-        )
-    } else {
-        return (
-            <div className={classes} {...props} />
-        )
-    }
+  if (card_header !== null || card_body !== null) {
+    return (
+      <Component className={classes} {...props}>
+        {card_header}
+        {card_body}
+      </Component>
+    );
+  } else {
+    return <Component className={classes} {...props} />;
+  }
 };
 
 Card.propTypes = {
+  body: PropTypes.node,
+  title: PropTypes.string,
   as: PropTypes.oneOfType([PropTypes.node, PropTypes.func])
 };
 
 Card.defaultProps = {
+  body: null,
+  title: "",
   as: "div"
 };
 
 export default Card;
-
-Card.propTypes = {
-    body: PropTypes.node,
-    title: PropTypes.string,
-};
-
-Card.defaultProps = {
-    body: null,
-    title: ''
-};
